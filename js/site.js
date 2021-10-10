b4a53c652686001dcc0e9c9d82549648a110f32f
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-10 00:38:20 -0400

    done

--- js/site.js
+++ js/site.js
@@ -1,31 +1,71 @@
-function displayMessage() {
-    let msg = "";
+function getValues() {
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    msg = document.getElementById("message").value;
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+    
+    let errorState = false;
+    let errorMsg = "";
 
-    alert(msg);
+
+    if (isNaN(fizzValue) || isNaN(buzzValue)) {
+        errorState = true;
+        errorMsg += "Please use numbers<hr/>";
+
+    }
+
+   
+    if (fizzValue > 200 || fizzValue < 1 || buzzValue > 200 || buzzValue < 1) {
+        errorState = true;
+        errorMsg += "1 and 200 are the limit<hr/>"
+    }
+
+    if (errorState) {
+        Swal.fire(
+            'Something went wrong',
+            `${errorMsg}`,
+            'error'
+        )
+        return;
+    }
 }
 
+function fizzBuzz(fizzValue, buzzValue) {
+    let returnArray = [];
 
-function displayMessage2() {
-    let msg = "";
+    for (let index = 1; index <= 100; index++) {
 
-    msg = document.getElementById("message").value;
-
-    Swal.fire(msg);
+        if (index % fizzValue == 0 && index % buzzValue == 0) {
+            returnArray.push('FizzBuzz');
+        } else if (index % fizzValue == 0) {
+            returnArray.push('Fizz');
+        } else if (index % buzzValue == 0) {
+            returnArray.push('Buzz');
+        } else {
+            returnArray.push(index);
+        }
+    }
+    return returnArray;
 }
 
+function displayData(fbArray) {
+    let tableBody = document.getElementById('results');
 
-// Two functions
-// function displayMessage(msg) {
+    let templateRow = document.getElementById('fbTemplate');
 
-//     alert(msg);
-// }
+    tableBody.innerHTML = "";
 
-// function getMessage() {
-//     let msg = "";
+    for (let index = 0; index < fbArray.length; index += 5) {
+        let tableRow = document.importNode(templateRow.content, true);
 
-//     msg = document.getElementById("message").value;
+        let rowCols = tableRow.querySelectorAll('td');
+        rowCols[0].textContent = fbArray[index];
+        rowCols[1].textContent = fbArray[index + 1];
+        rowCols[2].textContent = fbArray[index + 2];
+        rowCols[3].textContent = fbArray[index + 3];
+        rowCols[4].textContent = fbArray[index + 4];
 
-//     displayMessage(msg);
-// }
+        tableBody.appendChild(tableRow);
+    }
+}
